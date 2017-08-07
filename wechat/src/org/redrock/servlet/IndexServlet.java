24a4fa0bc9f9bbd942bd113ff2d45aaedd68889e
honeyfunnybunny <1530417351@qq.com>
2017-08-08 00:15:02 +0800

    加解密

--- wechat/src/org/redrock/servlet/IndexServlet.java
+++ wechat/src/org/redrock/servlet/IndexServlet.java
@@ -1,5 +1,7 @@
 package org.redrock.servlet;
 
+import com.qq.weixin.mp.aes.AesException;
+import com.qq.weixin.mp.aes.WXBizMsgCrypt;
 import org.redrock.util.Const;
 import org.redrock.util.EncryptUtil;
 import org.redrock.util.StringUtil;
@@ -7,6 +9,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.servlet.ServletException;
@@ -17,39 +20,84 @@ import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+
 //配置路由
-@WebServlet(name = "IndexServlet", value = "/")
+//@WebServlet(name = "IndexServlet", value = "/")
 public class IndexServlet extends HttpServlet {
     //post请求处理
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            //从request请求中获取输入流
-            InputStream inputStream = request.getInputStream();
-            //通过输入流获取Document对象
+            Map<String, String[]> params = request.getParameterMap();
+//            for (String param : params.keySet()) {
+//                System.out.println(param);
+//            }
+            String encodingAesKey = Const.EncodingAESKey;
+            String token = Const.Token;
+            String appId = Const.AppId;
+            String timestamp = request.getParameter("timestamp");
+            String nonce = request.getParameter("nonce");
+            String msgSignature = request.getParameter("msg_signature");
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(
+                            request.getInputStream(), "UTF-8"
+                    )
+            );
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+            String encryptMsg = builder.toString();
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = null;
-            builder = factory.newDocumentBuilder();
-            Document document = builder.parse(inputStream);
-            //<xml>
-            //  <ToUserName><![CDATA[gh_b6a171776f25]]></ToUserName>
-            //  <FromUserName><![CDATA[oiL6j0WJxy7Nagpnt6rX7Yo_5LeM]]></FromUserName><CreateTime>1501741106</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[emmm]]></Content><MsgId>6449928937743195428</MsgId></xml>
+            DocumentBuilder builder1 = factory.newDocumentBuilder();
+            Document document = builder1.parse(
+                    new InputSource(
+                            new StringReader(encryptMsg)
+                    )
+            );
+            Element element = document.getDocumentElement();
+            NodeList nodeList = element.getChildNodes();
+            Map<String, String> result = new HashMap<>();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (!node.getNodeName().equals("#text")) {
+                    String nName = node.getNodeName();
+                    String nValue = node.getTextContent();
+                    result.put(nName, nValue);
+                }
+            }
+            String toUserName = result.get("ToUserName");
+            String encrypt = result.get("Encrypt");
+            String format = "<xml><ToUserName><![CDATA[%s]]></ToUserName><Encrypt><![CDATA[%s]]></Encrypt></xml>";
+            String fromXML = String.format(format, toUserName, encrypt);
+            WXBizMsgCrypt pc = new WXBizMsgCrypt(token, encodingAesKey, appId);
+            String result2 = pc.decryptMsg(msgSignature, timestamp, nonce, fromXML);
+            //System.out.println("解密后明文: " + result2);
+
+            DocumentBuilderFactory factory2 = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder2 = null;
+            builder2 = factory2.newDocumentBuilder();
+            Document document2 = builder2.parse(
+                    new InputSource(new StringReader(result2))
+            );
+
             //获取根节点
-            Element rootNode = document.getDocumentElement();
+            Element rootNode = document2.getDocumentElement();
             //根节点名
             String name = rootNode.getNodeName();
             //获取子节点数组
             NodeList items = rootNode.getChildNodes();
-            Map<String, String> result = new HashMap<>();
+            Map<String, String> result3 = new HashMap<>();
             //子节点遍历
             for (int i = 0; i < items.getLength(); i++) {
                 Node item = items.item(i);
                 String iName = item.getNodeName();
+//                System.out.println(iName);
                 //<ToUserName><![CDATA[gh_b6a171776f25]]></ToUserName>
                 //注意：ToUserName标签内部的文本内容实际上也是一个节点，这里不能通过getNodeValue直接获取节点内容
                 String value = item.getTextContent();
@@ -58,8 +106,14 @@ public class IndexServlet extends HttpServlet {
                 }
                 result.put(iName, value);
             }
-            String Content = result.get("Content");
-            if (Content.equals("1")) {
+
+
+            //遍历result2
+            String toUser = result.get("FromUserName");
+
+            System.out.println(toUser);
+            if(encrypt == null || encrypt.equals("raw")){
+
                 //xml格式化
                 String xml = "<xml>" +
                         "<ToUserName><![CDATA[%s]]></ToUserName>" +
@@ -68,7 +122,6 @@ public class IndexServlet extends HttpServlet {
                         "<MsgType><![CDATA[%s]]></MsgType>" +
                         "<Content><![CDATA[%s]]></Content>" +
                         "</xml>";
-                String toUser = result.get("FromUserName");
                 String fromUser = result.get("ToUserName");
                 String createTime = System.currentTimeMillis() / 1000 + "";
                 String msgType = "text";
@@ -77,13 +130,15 @@ public class IndexServlet extends HttpServlet {
                 String res =String.format(xml, toUser, fromUser, createTime, msgType, content);
                 //response相应输出
                 response.getWriter().println(res);
-            } else {
-
             }
+            else{
 
+            };
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (AesException e) {
             e.printStackTrace();
         }
     }
--- wechat/src/org/redrock/util/Const.java
+++ wechat/src/org/redrock/util/Const.java
@@ -1,14 +1,14 @@
-package org.redrock.util;
+package util;
 
 /**
  * 微信公众号管理界面配置参数
  */
 public class Const {
 
-    public static final String AppId = "xxx";
-    public static final String AppSecret = "xxx";
+    public static final String AppId = "wx9825405b418c4a79";
+    public static final String AppSecret = "b958d3db244ae84d870ec66ce4f1a3ae";
     public static final String Token = "xxx";
     //测试账号暂时不用配置
 
-    //public static final String EncodingAESKey = "xxx";
+    public static final String EncodingAESKey = "g03cJU585bdbs8sVlN0k5wOCkSSylFNRJ1l123JntY3";
 }
